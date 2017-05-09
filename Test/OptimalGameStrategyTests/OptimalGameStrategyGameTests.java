81aedd78bc52cfc4c022f94342604817927ec49b
MichaelJamesParsons <mjay.parsons@gmail.com>
2017-05-08 21:35:11 -0400

    Fixed bug in optimal game strategy

--- Test/OptimalGameStrategyTests/OptimalGameStrategyGameTests.java
+++ Test/OptimalGameStrategyTests/OptimalGameStrategyGameTests.java
@@ -1,10 +1,11 @@
 package OptimalGameStrategyTests;
 
 import OptimalGameStrategy.OptimalGameStrategy;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-public class OptimalGameStrategyGameTests {
+class OptimalGameStrategyGameTests {
     private OptimalGameStrategy strategy;
 
     @BeforeEach
@@ -12,10 +13,15 @@ public class OptimalGameStrategyGameTests {
         strategy = new OptimalGameStrategy();
     }
 
+    /**
+     * Example from instructional video.
+     * @link https://www.youtube.com/watch?v=WxpIHvsu1RI
+     * @throws Exception
+     */
     @Test
     void testProblemSet1() throws Exception {
         int[] coins = {3,9,1,2};
-        strategy.calculate(coins);
+        Assertions.assertEquals(strategy.calculate(coins), 5);
     }
 
 }
--- src/OptimalGameStrategy/OptimalGameStrategy.java
+++ src/OptimalGameStrategy/OptimalGameStrategy.java
@@ -2,34 +2,25 @@ package OptimalGameStrategy;
 
 public class OptimalGameStrategy {
 
-    public ScoreSet calculate(int[] coins) throws Exception {
+    public int calculate(int[] coins) throws Exception {
         if(coins.length % 2 != 0) {
             throw new Exception("Coins must contain an even number of items.");
         }
 
-        ScoreSet[][] matrix = new ScoreSet[coins.length][coins.length];
-
-        for(int x = 0; x < coins.length; x++) {
-            matrix[x][x] = new ScoreSet(coins[x], 0);
-        }
+        int[][] matrix = new int[coins.length][coins.length];
 
         for(int d = 1; d < coins.length; d++) {
             for(int i = 0; i < coins.length-d; i++) {
                 int j = i+d;
 
-                //int pick3 = Math.min(matrix[i+1][j-1], matrix[i+2][j]);
+                //pick Vi first
+                int pick1 = (i+1 < j-1) ? matrix[i+2][j] : 0;
+                int pick2 = (i <= j-2) ? matrix[i+1][j-1] : 0;
 
-                int pick1 = matrix[i+1][j].getFirstMove() + matrix[i][j-1].getSecondMove();
-                int pick2 = matrix[i][j-1].getFirstMove() + matrix[i+1][j].getSecondMove();
-                ScoreSet score;
+                //pick Vj first
+                int pick3 = (i+1 < j-1) ? matrix[i][j-2] : 0;
 
-                if(pick1 > pick2) {
-                    score = new ScoreSet(matrix[i+1][j].getFirstMove(), matrix[i][j-1].getFirstMove());
-                } else {
-                    score = new ScoreSet(matrix[i][j-1].getFirstMove(), matrix[i+1][j].getFirstMove());
-                }
-
-                matrix[i][j] = score;
+                matrix[i][j] = Math.max(Math.min(pick1, pick2) + coins[i], Math.min(pick1, pick3) + coins[j]);
             }
         }
 
